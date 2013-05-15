1109586f59cf91f30c0742ae8802dc645a319ee5
Michael Raimondi <zeno@pobox.com>
2013-05-15 16:08:59 -0400

    Completed 182/280

--- triangle.rb
+++ triangle.rb
@@ -13,8 +13,23 @@
 # and
 #   about_triangle_project_2.rb
 #
+
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == 0 && b == 0 && c == 0
+    raise TriangleError
+  elsif a < 0 || b < 0 || c < 0
+    raise TriangleError
+  elsif a+b <= c || b+c <= a || a+c <= b
+    raise TriangleError
+  end
+
+  if a == b && b == c && a == c
+    :equilateral
+  elsif a == b || b == c || a == c
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
